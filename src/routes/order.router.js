70435e79f706248103e8546e742a15ee28a19359
jovid_18 <cshcho99@gmail.com>
2024-02-29 14:04:10 +0900

    FEAT: add order.router.js

---
+++ src/routes/order.router.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import Joi from 'joi';
+import { prisma } from '../utils/prisma/index.js';
+import { ownerauth, customerauth } from '../middlewares/auth.middleware.js';
+
+const router = express.Router();
+
+//1, 2번 joi 필요시 작성
+
+//3, 4 번 joi 필요시 작성
+
+// 1번 라우터 작성
+
+// 2번 라우터 작성
+
+// 3번 라우터 작성
+
+// 4번 라우터 작성
+export default router;
